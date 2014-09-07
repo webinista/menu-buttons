88ef4e7554622d72fc490d8d85a252c1cf68abf5
tiffany <tiff@webinista.local>
2014-09-06 22:25:01 -0700

    Intial file set.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-menu-buttons
+Menu Buttons
 ============
 
 A collection of interactions for menu buttons, some original. Most based from other places on the web.
---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    'use strict';
+    $('.svgwrap').click(function (e) {
+        $(e.currentTarget).toggleClass('open');
+    }); 
+});
